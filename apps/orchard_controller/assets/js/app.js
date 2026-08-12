164f5d18998de8e731898e672c30db7b220d659b
Najib Ninaba <najibninaba@gmail.com>
2026-08-12 08:03:52 +0800

    fix(console): remove unsupported LiveView long-poll fallback

--- apps/orchard_controller/assets/js/app.js
+++ apps/orchard_controller/assets/js/app.js
@@ -561,7 +561,6 @@ const FORMAT_OPTIONS = {
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
-  longPollFallbackMs: 2500,
   hooks: Hooks,
   params: {_csrf_token: csrfToken}
 })
